e983f86914cc081ee888dd450be301ecbe2a4e9d
Lydia <lydiadoula@gmail.com>
2023-04-08 16:38:50 +0100

    form added

---
+++ exptracker/client-frontend/src/Components/Form.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {useForm} from 'react-hook-form'; //get all the data of the form
+
+export default function Form() {
+
+    const {register, handleSubmit, resetField} = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data)
+        //data is parameter and can have any name
+    }
+    return (
+        <div className='form'>
+            <h1 className='formh1'>Transaction</h1>
+                                        
+            <form id='form' onSubmit={handleSubmit(onSubmit)}> 
+                <div className='grid'>
+                    <div className='input-group'>
+                        <input type='text' {...register('name')} placeholder='Salary, House rent, Dogsit' className='form-input'></input>
+                    </div>
+                    <select className='form-input' {...register('type')}>
+                        <option value='Investment' defaultValue>Investment</option>
+                        <option value='Expense' defaultValue>Expense</option>
+                        <option value='Savings' defaultValue>Savings</option>
+                    </select>
+                    <div className='input group'>
+                        <input type='text' {...register('amount')} placeholder='Amount' className='form-input'></input>
+                    </div>
+                    <div className='submitbtn'>
+                        <button className='submitbtn'>Submit</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
